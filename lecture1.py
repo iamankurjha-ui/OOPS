17184759ecac1736fa63e205c05bd58d936d7f8c
iamankurjha-ui <iamankurjha@gmail.com>
2026-03-12 19:18:06 +0530

    decorator

--- lecture1.py
+++ lecture1.py
@@ -12,7 +12,22 @@
 # print(res)
 
 
-from functools import reduce
-l=[1,2,3,4,5]
-res = reduce(lambda x,y : x if x<y else y,l)
-print(res)
+# from functools import reduce
+# l=[1,2,3,4,5]
+# res = reduce(lambda x,y : x if x<y else y,l)
+# print(res)
+
+
+#Decorator
+#What is a Decorator in Python?
+#A decorator is a function that modifies or extends the behavior 
+# of another function without changing its actual code.
+
+def outer(var):
+    def inner():
+        var()
+    return inner
+def show():
+    print("from show function")
+res = outer(show)
+res()
